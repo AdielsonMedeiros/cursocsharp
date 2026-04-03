39f859516bd5fa46884e16c8324b74e78ba7e68b
AdielsonMedeiros <medeirosadielson@gmail.com>
2026-04-03 13:40:20 -0300

    first commit

---
+++ teste/Program.cs
@@ -0,0 +1,19 @@
+// 'using System;' permite usar classes fundamentais do C#, como a 'Console'.
+using System;
+
+// 'namespace' é usado para organizar o código em pastas lógicas e evitar conflitos de nomes.
+namespace _01Conceitos
+{
+    // 'class Program' define uma classe, que é uma estrutura que contém o código.
+    class Program
+    {
+        // 'static void Main' é o "método principal" (ponto de partida). 
+        // O programa sempre começa a ser executado a partir desta linha.
+        static void Main(string[] args)
+        {
+            // 'Console.WriteLine' escreve uma linha de texto na tela do computador.
+            // "Hello, World!" é a mensagem que será exibida.
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
